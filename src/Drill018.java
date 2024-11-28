c37585ca7f984586a36bcab4d01e0d0999e22c07
Silvia <wangshining521@gmail.com>
2024-11-28 12:09:30 +0900

    break

---
+++ src/Drill018.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Drill018 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while (true){
+            int key = sc.next().charAt(0);
+            if (key == 'Q') break;
+        }
+    }
+}
